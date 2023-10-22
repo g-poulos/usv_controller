9415d9b3be632aaffcc754c8ded23b7d912f8709
g-poulos <cs04480@uoi.gr>
2023-10-22 19:30:48 +0300

    Fixed bag writer

--- usv_controller/bag_writer.py
+++ usv_controller/bag_writer.py
@@ -1,8 +1,8 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.serialization import serialize_message
-
-from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Vector3, Pose
+from nav_msgs.msg import Odometry
 import rosbag2_py
 
 
@@ -10,8 +10,10 @@ class SimpleBagRecorder(Node):
     def __init__(self):
         super().__init__('simple_bag_recorder')
         self.writer = rosbag2_py.SequentialWriter()
-        storage_options = rosbag2_py._storage.StorageOptions(uri='bagfile', storage_id='mcap')
-        converter_options = rosbag2_py._storage.ConverterOptions('', '')
+        storage_options = rosbag2_py._storage.StorageOptions(
+            uri='src/usv_controller/bagfiles/record5',
+            storage_id='mcap')
+        converter_options = rosbag2_py._storage.ConverterOptions('cdr', 'cdr')
         self.writer.open(storage_options, converter_options)
 
         wave_force_topic_info = rosbag2_py._storage.TopicMetadata(
@@ -26,17 +28,45 @@ class SimpleBagRecorder(Node):
             serialization_format='cdr')
         self.writer.create_topic(wave_torque_topic_info)
 
+        odom_topic_info = rosbag2_py._storage.TopicMetadata(
+            name='/model/boat/pose',
+            type='geometry_msgs/msg/Pose',
+            serialization_format='cdr')
+        self.writer.create_topic(odom_topic_info)
 
-        self.subscription = self.create_subscription(
+        self.wave_force_subscription = self.create_subscription(
             Vector3,
-            '/wave/force',
-            self.topic_callback,
+            "/wave/force",
+            self.wave_force_callback,
             10)
-        self.subscription
 
-    def topic_callback(self, msg):
+        self.wave_torque_subscription = self.create_subscription(
+            Vector3,
+            "/wave/torque",
+            self.wave_torque_callback,
+            10)
+
+        self.odom_subscription = self.create_subscription(
+            Pose,
+            "/model/boat/pose",
+            self.pose_callback,
+            10)
+
+    def wave_force_callback(self, msg):
         self.writer.write(
             '/wave/force',
+            serialize_message(msg),
+            self.get_clock().now().nanoseconds)
+
+    def wave_torque_callback(self, msg):
+        self.writer.write(
+            '/wave/torque',
+            serialize_message(msg),
+            self.get_clock().now().nanoseconds)
+
+    def pose_callback(self, msg):
+        self.writer.write(
+            "/model/boat/pose",
             serialize_message(msg),
             self.get_clock().now().nanoseconds)
 
