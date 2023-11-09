d9c10dc83440ab0347e689318c59ea96cfa69e32
g-poulos <cs04480@uoi.gr>
2023-11-09 23:03:17 +0200

    Added equations

--- diff_calc.py
+++ diff_calc.py
@@ -4,6 +4,7 @@ import random
 import matplotlib.pyplot as plt
 
 
+############### Dimensions and Mass ###############
 m = 425             # Mass of the structure
 R_uc = 0.22         # Upper cylinder radius
 H_uc = 0.65         # Upper cylinder height
@@ -17,12 +18,29 @@ d_ad = math.sqrt(L_ac ** 2 - (L_bc / 2) ** 2)
 d_bf = L_bc / 2
 d_ae = d_ad * 2 / 3
 
-
 I_zz = 2.82698738e+03
 
+############### Coefficients and Constants ###############
 density = 1025      # Water density
 Ca = 0.8            # Added mass coefficient
 Cd = 0.8            # Drag coefficient
+
+############### Initial Values ###############
+x_s = 5             # (m)     Start position in X axis
+y_s = 5             # (m)     Start position in Y axis
+psi_s = 10          # (deg)   Start direction
+u_s = 0.10          # (m/s)   Start surge velocity
+v_s = -0.10         # (m/s)   Start sway velocity
+r_s = 0.01          # (rad/s) Start yaw velocity
+
+
+############### Current Values ###############
+x = x_s
+y = y_s
+psi = psi_s
+u = u_s
+v = v_s
+r = r_s
 
 
 class IntegratedWhiteNoise:
@@ -47,7 +65,7 @@ def height_above_surface():     # (Eq. 4)
     return H_uc - (1/(R_uc**2)) * (m/(3 * math.pi * density) - (R_lc**2)*H_lc)
 
 
-def force_on_a(h, ):              # (Eq. 5a)
+def force_on_a(h, ):            # (Eq. 5a)
     # TODO: Add velocities and accelerations
     added_mass_force = Ca * math.pi * density * ((R_uc**2) * (H_uc - h) + (R_lc**2)*H_lc)
     inertia_force = math.pi * density * ((R_uc**2) * (H_uc - h) + (R_lc**2)*H_lc)
@@ -56,13 +74,63 @@ def force_on_a(h, ):              # (Eq. 5a)
     return added_mass_force + inertia_force + drag_force
 
 
+def velocity_a():        # (Eq. 3a)
+    x_vel = u + r * ((L_bc/2) - d_bf)
+    y_vel = v + r * d_ae
+    return x_vel, y_vel
+
+
+def position_a():
+    # Initial
+    h = 0.01
+    x_pos = d_ae
+    y_pos = d_bf - (L_bc/2)
+
+    # First step
+    x_v, y_v = velocity_a()
+    x_pos_temp = x_pos
+    y_pos_temp = y_pos
+
+    # Continue
+    x = [x_pos]
+    y = [y_pos]
+    for i in range(1000):
+        x_pos_temp = x_pos_temp + h * x_v
+        y_pos_temp = y_pos_temp + h * y_v
+        x.append(x_pos_temp)
+        y.append(y_pos_temp)
+
+    return x, y
+
+
+def position():
+    x_dot = np.transpose(np.array([x, y, psi]))
+    rotation = np.array([[np.cos(psi), -np.sin(psi), 0],
+                        [np.sin(psi), np.cos(psi), 0],
+                        [0, 0, 1]])
+    velocity = np.transpose(np.array([u, v, r]))
+    print(x_dot)
+    print(rotation)
+    print(velocity)
+
+
 if __name__ == '__main__':
-    dist = IntegratedWhiteNoise(0, 10, 0.1)
+    current_velocity = IntegratedWhiteNoise(0, 0.514, 0.001)
+    current_direction = IntegratedWhiteNoise(0, 360, 1)
+    wind_velocity = IntegratedWhiteNoise(0, 7.716, 0.04)
+    wind_direction = IntegratedWhiteNoise(0, 360, 1)
 
-    r = 1000000
-    x = []
-    for i in range(r):
-        x.append(dist.get_value())
+    # r = 1000000
+    # cv = []
+    # cd = []
+    # wv = []
+    # wd = []
+    # for i in range(r):
+    #     cv.append(current_velocity.get_value())
+    #     wv.append(wind_velocity.get_value())
+    #
+    # plt.plot(range(r), cv)
+    # plt.plot(range(r), wv)
+    # plt.show()
 
-    plt.plot(range(r), x)
-    plt.show()
+    position()
