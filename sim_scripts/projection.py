142f8c15d179307985f73c79f7213694e7609cf2
g-poulos <cs04480@uoi.gr>
2023-11-23 12:49:04 +0200

    Projection works

--- sim_scripts/projection.py
+++ sim_scripts/projection.py
@@ -1,89 +1,13 @@
 import pyvista as pv
 import numpy as np
-from pymeshfix import _meshfix
-import pymeshfix as mf
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.spatial import Delaunay
+import vtk
 
-
-def area(x1, y1, x2, y2, x3, y3):
-    return abs((x1 * (y2 - y3) + x2 * (y3 - y1)
-                + x3 * (y1 - y2)) / 2.0)
-
-
-# A function to check whether point P(x, y)
-# lies inside the triangle formed by
-# A(x1, y1), B(x2, y2) and C(x3, y3)
-def isInside(triangle, x, y):
-    x1 = triangle[0][0]
-    y1 = triangle[0][1]
-    x2 = triangle[1][0]
-    y2 = triangle[1][1]
-    x3 = triangle[2][0]
-    y3 = triangle[2][1]
-
-    A = area(x1, y1, x2, y2, x3, y3)
-    A1 = area(x, y, x2, y2, x3, y3)
-    A2 = area(x1, y1, x, y, x3, y3)
-    A3 = area(x1, y1, x2, y2, x, y)
-
-    if A == A1 + A2 + A3:
-        return True
-    else:
-        return False
-
-
-def get_face_points(face, points):
-    p1 = points[face[1], :]
-    p2 = points[face[2], :]
-    p3 = points[face[3], :]
-    return [p1, p2, p3]
-
-
-# def remove_duplicates():
-#     # projection.plot(show_edges=True, line_width=2)
-#     triang = projection.triangulate().clean()
-#     #
-#     faces = triang.faces.reshape(-1, 4)
-#     points = triang.points
-#
-#     triangle = get_face_points(faces[0], points)
-#
-#     faces_to_remove = []
-#     i = 0
-#     for face in faces:
-#         f_points = get_face_points(face, points)
-#
-#         b = True
-#         for p in f_points:
-#             b = isInside(triangle, p[0], p[1])
-#             if not b:
-#                 print("Xaxa")
-#         if b:
-#             faces_to_remove.append(i)
-#             print(f"Face: {face} is duplicate")
-#
-#         b = True
-#         i = i + 1
-#     print(len(faces_to_remove))
-#     n = triang.remove_cells(faces_to_remove)
-#     print(triang)
-#     print(n)
-#     # print(n)
-
-
-# def project_points_to_plane(points, plane_origin, plane_normal):
-#     """Project points to a plane."""
-#     vec = points - plane_origin
-#     dist = np.dot(vec, plane_normal)
-#     return points - np.outer(dist, plane_normal)
 
 def project_points_to_plane(mesh, origin=None, normal=(1, 0, 0), inplace=False):
     """Project points of this mesh to a plane"""
     if not isinstance(mesh, (pv.PolyData)):
         raise TypeError('Please use surface meshes only.')
-    import vtk
+
     if origin is None:
         origin = mesh.center
     if not inplace:
@@ -102,47 +26,41 @@ def project_points_to_plane(mesh, origin=None, normal=(1, 0, 0), inplace=False):
     return
 
 
-def get_projection_area(mesh):
-    # Define a plane
-    origin = [0, 0, 0]
-    normal = [0, 1, 0]
-
+def get_projection_area(mesh, normal, iterations=10, plot=False):
     min_z = mesh.bounds[4]
-    np.linspace(-0.67, 0, 10)
+    max_z = mesh.bounds[5]
+    step = (max_z - min_z)/iterations
+    total_area = 0
 
-    for i in range(3):
-        print(i)
-        step = 0.11
-
+    for i in range(iterations):
+        # Take mesh slice
         clipped = vereniki.clip('z', value=(i+1)*step, origin=(0, 0, min_z))
         clipped = clipped.clip('z', value=i*step, origin=(0, 0, min_z), invert=False)
 
-        projected_points = project_points_to_plane(clipped, origin, normal).points
-        print(projected_points.shape)
-        print(np.unique(projected_points, axis=0).shape)
+        # Project slice to plane and remove duplicate points
+        projected_points = project_points_to_plane(clipped, normal=normal).points
         projected_points = np.unique(projected_points, axis=0)
 
-        # Create a polydata object with projected points
+        # Generate new surface from projected points and add its area
         polydata = pv.PolyData(projected_points)
+        surface = polydata.delaunay_2d(alpha=0.3)
+        total_area = total_area + surface.area
 
-        # Mesh using delaunay_2d and pyvista
-        surface = polydata.delaunay_2d(alpha=0.5)
-
-
-        # plot it
-        pl = pv.Plotter()
-        pl.add_mesh(mesh, style='wireframe')
-        pl.add_mesh(surface, show_edges=True, color='white', opacity=0.5, label='Tessellated mesh')
-        pl.add_mesh(
-            pv.PolyData(clipped.points),
-            color='red',
-            render_points_as_spheres=True,
-            point_size=5,
-            label='Points to project',
-        )
-        pl.add_legend()
-        pl.show()
-        print(surface.area)
+        if plot:
+            # plot it
+            pl = pv.Plotter()
+            pl.add_mesh(mesh, style='wireframe')
+            pl.add_mesh(surface, show_edges=True, color='white', opacity=0.5, label='New surface')
+            pl.add_mesh(
+                pv.PolyData(clipped.points),
+                color='red',
+                render_points_as_spheres=True,
+                point_size=5,
+                label='Points to project',
+            )
+            pl.add_legend()
+            pl.show()
+    return total_area
 
 
 if __name__ == '__main__':
@@ -155,36 +73,9 @@ if __name__ == '__main__':
     # vereniki_lower_part = vereniki.clip('z', value=-((0.65 + 0.3) / 2) + 0.3, invert=True)
     # vereniki_upper_part = vereniki.clip('z', value=-((0.65 + 0.3) / 2) + 0.45, invert=False)
 
-    get_projection_area(vereniki_upper_part)
+    area = get_projection_area(vereniki_lower_part, [1, 0, 0])
+    print(area)
 
-    # min_z = vereniki.bounds[4]
-    # print(min_z)
-    # origin = [0, 0, 0]
-    # normal = [0, 1, 0]
-    # projected_points = project_points_to_plane(vereniki.points, origin, normal)
-    # print(projected_points.shape)
-    # print(np.unique(projected_points, axis=0).shape)
-    # projected_points = np.unique(projected_points, axis=0)
-    #
-    #
-    # clipped = vereniki.clip('z', value=0.4, origin=(0, 0, min_z))
-    # clipped = clipped.clip('z', value=0.1, origin=(0, 0, min_z), invert=False)
-    #
-    # clipped_p = clipped.points
-    #
-    # p = pv.Plotter()
-    # # p.add_mesh(vereniki, style='wireframe', color='blue', label='Input')
-    # # p.add_mesh(box, opacity=0.3)
-    # p.add_mesh(clipped, label='Clipped')
-    # p.add_mesh(
-    #     pv.PolyData(clipped_p),
-    #     color='red',
-    #     render_points_as_spheres=True,
-    #     point_size=5,
-    #     label='Points to project',
-    # )
-    # p.add_legend()
-    # p.show()
 
 
 
