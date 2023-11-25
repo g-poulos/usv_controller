de36a2a5c91dbc4a7c7fe9350122a696df475d70
g-poulos <cs04480@uoi.gr>
2023-11-25 18:41:14 +0200

    Added plot functions + force_on_point

--- sim_scripts/diff_calc.py
+++ sim_scripts/diff_calc.py
@@ -10,7 +10,7 @@ def height_above_surface():     # (Eq. 4)
     return H_uc - (1/(R_uc**2)) * (m / (3 * math.pi * water_density) - (R_lc ** 2) * H_lc)
 
 
-def dist_to_force(speed, direction, velocity, mode):   # (Eq. 15)
+def dist_to_force(speed, direction, model_velocity, mode):   # (Eq. 15)
     C = 1
     relative_angle = direction - psi
 
@@ -26,7 +26,7 @@ def dist_to_force(speed, direction, velocity, mode):   # (Eq. 15)
         print("Invalid force mode")
         return
 
-    relative_velocity = vector_to_xy_components(speed, direction) - velocity[:2]
+    relative_velocity = model_velocity[:2] - vector_to_xy_components(speed, direction)
     magnitude = np.linalg.norm(relative_velocity)
     force_x = 0.5 * C * density * (magnitude**2) * At
     force_y = 0.5 * C * density * (magnitude**2) * Al
@@ -45,6 +45,15 @@ def get_rotation_matrix(angle):
     return np.array([[np.cos(angle), -np.sin(angle), 0],
                      [np.sin(angle), np.cos(angle), 0],
                      [0, 0, 1]])
+
+
+def force_on_point(acc):
+    # TODO: Add velocities and accelerations
+    added_mass_force = Ca * math.pi * water_density * ((R_uc ** 2) * (H_uc - h) + (R_lc ** 2) * H_lc)
+    inertia_force = math.pi * water_density * ((R_uc ** 2) * (H_uc - h) + (R_lc ** 2) * H_lc)
+    drag_force = Cd * water_density * (R_uc * (H_uc - h) + R_lc * H_lc)
+
+    return added_mass_force + inertia_force + drag_force
 
 
 # (Eq. 8f)
@@ -66,54 +75,87 @@ def get_engine_vectored_thrust(i):
     # return np.array([100, 100, 0])
 
 
-if __name__ == '__main__':
-    current_velocity = IntegratedWhiteNoise(0, 0.514, 0.1, 0.001)
-    current_direction = IntegratedWhiteNoise(0, 360, 100, 1)
-    wind_velocity = IntegratedWhiteNoise(0, 7.716, 2, 2)
-    wind_direction = IntegratedWhiteNoise(0, 360, 200, 7)
-    mass_inv = np.linalg.inv(get_mass_matrix())
-
-    engine_thrust = np.array([100, 100, 0])
-
-    h = 0.01
-    it = 1000
-
-    vel = np.zeros((3, it))
-    pos = np.zeros((3, it))
-
-    for i in range(it - 1):
-        wind_speed = wind_velocity.get_value()
-        wind_dir = wind_direction.get_value()
-        wind = dist_to_force(wind_speed, wind_dir, vel[:, i], mode="wind")
-
-        current_speed = current_velocity.get_value()
-        current_dir = current_direction.get_value()
-        current = dist_to_force(current_speed, current_dir, vel[:, i], mode="current")
-
-        acting_forces = engine_thrust + wind
-
-        vel[:, i + 1] = vel[:, i] + h * mass_inv.dot(acting_forces)             # Velocity
-        pos[:, i + 1] = pos[:, i] + h * mass_inv.dot(acting_forces) * (i * h)   # Position
-
-    fig, ax = plt.subplots(2, 3, sharex=True, sharey=True)
+def plot_pos_vel_acc(pos, vel, acc):
+    fig, ax = plt.subplots(3, 3, sharex=True)
     ax[0, 0].plot(range(it), pos[0, :])
     ax[0, 1].plot(range(it), pos[1, :])
     ax[0, 2].plot(range(it), pos[2, :])
-
     ax[1, 0].plot(range(it), vel[0, :])
     ax[1, 1].plot(range(it), vel[1, :])
     ax[1, 2].plot(range(it), vel[2, :])
+    ax[2, 0].plot(range(it), acc[0, :])
+    ax[2, 1].plot(range(it), acc[1, :])
+    ax[2, 2].plot(range(it), acc[2, :])
     ax[0, 0].set_title('Position X-Axis')
     ax[0, 1].set_title('Position Y-Axis')
     ax[0, 2].set_title('Orientation Z-Axis')
     ax[1, 0].set_title('Linear Velocity X-Axis')
     ax[1, 1].set_title('Linear Velocity Y-Axis')
     ax[1, 2].set_title('Angular Velocity Z-Axis')
-
-    for i in range(2):
+    for i in range(3):
         for j in range(3):
             ax[i, j].grid(True)
-    fig.set_figwidth(15)
+    fig.set_figwidth(16)
+    fig.set_figheight(9)
     plt.show()
+
+
+def plot_dist(current, wind):
+    fig, ax = plt.subplots(2, 3, sharex=True)
+    ax[0, 0].plot(range(it), current[0, :])
+    ax[0, 1].plot(range(it), current[1, :])
+    ax[0, 2].plot(range(it), current[2, :])
+    ax[1, 0].plot(range(it), wind[0, :])
+    ax[1, 1].plot(range(it), wind[1, :])
+    ax[1, 2].plot(range(it), wind[2, :])
+
+    fig.set_figwidth(16)
+    fig.set_figheight(9)
+    plt.show()
+
+
+if __name__ == '__main__':
+    current_velocity = IntegratedWhiteNoise(0, 0.514, 0.1, 1)
+    current_direction = IntegratedWhiteNoise(0, 360, 140, 6)
+    wind_velocity = IntegratedWhiteNoise(0, 7.716, 2, 2)
+    wind_direction = IntegratedWhiteNoise(0, 360, 270, 6)
+    mass_inv = np.linalg.inv(get_mass_matrix())
+
+    engine_thrust = np.array([10, 10, 0])
+
+    h = 0.01
+    it = int((0.01 * 100) * 3600)
+
+    acc = np.zeros((3, it))
+    vel = np.zeros((3, it))
+    pos = np.zeros((3, it))
+    current = np.zeros((3, it))
+    wind = np.zeros((3, it))
+
+    for i in range(it - 1):
+        wind_speed = wind_velocity.get_value()
+        wind_dir = wind_direction.get_value()
+        wind[:, i] = dist_to_force(wind_speed, wind_dir, vel[:, i], mode="wind")
+
+        current_speed = current_velocity.get_value()
+        current_dir = current_direction.get_value()
+        current[:, i] = dist_to_force(current_speed, current_dir, vel[:, i], mode="current")
+
+        acting_forces = engine_thrust + wind[:, i]
+        acc[:, i + 1] = mass_inv.dot(acting_forces)                             # Acceleration
+        vel[:, i + 1] = vel[:, i] + h * mass_inv.dot(acting_forces)             # Velocity
+        pos[:, i + 1] = pos[:, i] + h * mass_inv.dot(acting_forces) * (i * h)   # Position
+
+    plot_pos_vel_acc(pos, vel, acc)
+    plot_dist(current, wind)
+
+    # fig, ax = plt.subplots(2, 1, sharex=True)
+    # ax[0].plot(cv, label='Current Speed')
+    # ax[0].set_ylabel('Speed (m/s)')
+    #
+    # ax[1].plot(cd, label='Current Direction')
+    # ax[1].set_ylabel('Direction (degrees)')
+    # plt.show()
+
 
 
