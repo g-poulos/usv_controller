3676f45dc61fd947b85e183e3a92939f691ca288
g-poulos <cs04480@uoi.gr>
2023-12-08 20:23:23 +0200

    Added same disturbances with gazebo simulation

--- sim_scripts/disturbances.py
+++ sim_scripts/disturbances.py
@@ -1,6 +1,7 @@
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+from constants import *
 
 
 class IntegratedWhiteNoise:
@@ -15,7 +16,7 @@ class IntegratedWhiteNoise:
         next_value = self.prev_val + self.dt * random.gauss(0, self.stddev)
 
         if next_value > self.max_val or next_value < self.min_val:
-            next_value = next_value - self.dt * random.gauss(0, self.stddev)
+            next_value = self.prev_val - self.dt * random.gauss(0, self.stddev)
 
         self.prev_val = next_value
         return next_value
@@ -66,64 +67,80 @@ def find_closest(arr, target):
     return left
 
 
-def calculate_wrench(model_pose, model_vel, force_speed, force_direction, wrench_info):
-
+def calculate_wrench(model_pose, model_vel, force_speed, force_direction, wrench_info, mode):
     force_vel = np.array([force_speed * np.cos(force_direction),
                           force_speed * np.sin(force_direction)])
+    if mode == "wind":
+        fluid_dens = air_density
+    else:
+        fluid_dens = water_density
 
-    relative_vel = force_vel - model_vel
-    relative_angle = force_direction - model_pose[2]
-    table_index = find_closest(wrench_info.angle, force_direction)
+    relative_vel = force_vel - model_vel[:2]
+    relative_angle = (force_direction * (np.pi/180)) - model_pose[2]
+    index = find_closest(wrench_info.angle, relative_angle)
 
-    print(force_direction)
-    print(model_pose[2])
-    print(relative_angle)
+    force_surface = wrench_info.force_area[index]
+    torque_surface = wrench_info.torque_area[index]
+    offset = wrench_info.offset[index]
+
+    # Force
+    force = 0.5 * fluid_dens * Cd * relative_vel * np.linalg.norm(relative_vel) * force_surface
+
+    # Torque
+    torque_force = 0.5 * fluid_dens * Cd * relative_vel * np.linalg.norm(relative_vel) * torque_surface
+    torque = np.linalg.norm(torque_force) * offset
+
+    return np.array([force[0], force[1], torque])
 
 
 def test_dist():
     current_velocity = IntegratedWhiteNoise(0, 0.514, 0.1, 0.001)
     current_direction = IntegratedWhiteNoise(0, 360, 100, 1)
-    wind_velocity = IntegratedWhiteNoise(0, 7.716, 2, 2)
-    wind_direction = IntegratedWhiteNoise(0, 360, 200, 25)
+    wind_velocity = IntegratedWhiteNoise(0, 10, 3, 0.3)
+    wind_direction = IntegratedWhiteNoise(200, 360, 200, 10)
 
-    #      one minute           * x
-    time = int(0.01 * 100 * 60) * 60
-    cv = []
-    cd = []
-    wv = []
-    wd = []
-    f = []
+    wind_info = read_csv("wind_table.csv")
+    current_info = read_csv("current_table.csv")
+
+    minutes = 2
+    plot_dist(minutes, wind_direction, wind_velocity, wind_info, mode="wind")
+    plot_dist(minutes, current_direction, current_velocity, current_info, mode="current")
+
+
+def plot_dist(minutes, direction_distribution, speed_distribution, wrench_info, mode):
+    time = int(100 * 60) * minutes
+    speed = []
+    direction = []
+    f = np.zeros((3, time))
     for i in range(time):
-        vel = wind_velocity.get_value()
-        dir = wind_direction.get_value()
-        wv.append(vel)
-        wd.append(dir)
-        vel = current_velocity.get_value()
-        dir = current_direction.get_value()
-        cv.append(vel)
-        cd.append(dir)
-
-    fig, ax = plt.subplots(4, 1, sharex=True)
-    ax[0].plot(wv, label='Wind Speed')
+        speed_val = speed_distribution.get_value()
+        direction_val = direction_distribution.get_value()
+        f[:, i] = calculate_wrench(np.array([0, 0, 0]), np.array([0, 0, 0]),
+                                   speed_val, direction_val, wrench_info, mode=mode)
+        speed.append(speed_val)
+        direction.append(direction_val)
+    fig, ax = plt.subplots(5, 1, sharex=True)
+    ax[0].plot(speed, label='Wind Speed')
     ax[0].set_ylabel('Speed (m/s)')
-
-    ax[1].plot(wd, label='Wind Direction')
+    ax[1].plot(direction, label='Wind Direction')
     ax[1].set_ylabel('Direction (degrees)')
-
-    ax[2].plot(cv, label='Current Speed')
-    ax[2].set_ylabel('Speed (m/s)')
-
-    ax[3].plot(cd, label='Current Direction')
-    ax[3].set_ylabel('Direction (degrees)')
-
+    ax[2].plot(f[0, :], label='X Force')
+    ax[2].set_ylabel('X Force')
+    ax[3].plot(f[1, :], label='Y Force')
+    ax[3].set_ylabel('Y Force')
+    ax[4].plot(f[2, :], label='Z Torque')
+    ax[4].set_ylabel('Z Torque')
+    ax[4].set_xlabel('Time (ms)')
     fig.set_figwidth(16)
     fig.set_figheight(9)
+    fig.suptitle(mode)
     plt.show()
 
 
 if __name__ == '__main__':
 
-    wrench_info = read_csv("wind_table.csv")
-    i = find_closest(wrench_info.angle, 0.56)
-    print(wrench_info)
-    print(wrench_info.angle[i])
+    test_dist()
+    # wrench_info = read_csv("wind_table.csv")
+    # i = find_closest(wrench_info.angle, 0.56)
+    # print(wrench_info)
+    # print(wrench_info.angle[i])
