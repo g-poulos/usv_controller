5769141c92c7d6445f678b8253f61188a83aee28
g-poulos <cs04480@uoi.gr>
2023-12-19 20:55:19 +0200

    Fixed vector to thrust conversion but needs '-' before atan2

--- usv_controller/vereniki_control.py
+++ usv_controller/vereniki_control.py
@@ -12,7 +12,7 @@ d_bf = L_bc / 2
 d_ae = d_ad * (2 / 3)
 
 
-def get_thrust_msgs(input_vec):
+def get_thrust_msgs2(input_vec):
     dv = (L_ac + L_bc) / 2
     J_star = np.array([[1, 0, -(d_bf - (L_bc / 2)) / dv],
                        [0, -1, -d_ae / dv],
@@ -24,6 +24,28 @@ def get_thrust_msgs(input_vec):
     J_star_plus = J_star @ np.linalg.inv(J_star.T @ J_star)
     q_star = input_vec
     return J_star_plus @ q_star.T
+
+
+def get_thrust_msgs(input_vec):
+    dv = (L_ac + L_bc) / 2
+    J_star = np.array([[1, 0, -(d_bf - (L_bc / 2)) / dv],
+                       [0, -1, -d_ae / dv],
+                       [1, 0, -d_bf / dv],
+                       [0, -1, (d_ad - d_ae) / dv],
+                       [1, 0, (L_bc - d_bf) / dv],
+                       [0, -1, (d_ad - d_ae) / dv]])
+    J_star = J_star.T
+
+    J_star_plus = J_star.T @ np.linalg.inv(J_star @ J_star.T)
+    # q_star = np.array([input_vec[0], input_vec[1], input_vec[2]/dv])
+    q_star = input_vec
+    return J_star_plus @ q_star
+
+
+def cartesian_to_polar(x, y):
+    r = np.sqrt(x**2 + y**2)
+    theta = -np.arctan2(y, x)
+    return r, theta
 
 
 def thrust_to_rotations(thrust):
@@ -57,7 +79,7 @@ class VerenikiControllerNode(Node):
         self.timer = self.create_timer(1, self.send_thrust_command)
 
     def send_thrust_command(self):
-        msgs = get_thrust_msgs(np.array([100, 0, 40]))
+        msgs = get_thrust_msgs(np.array([100, 0, 10]))
 
         thrustA_msg = Float64()
         directionA_msg = Float64()
@@ -66,12 +88,17 @@ class VerenikiControllerNode(Node):
         thrustC_msg = Float64()
         directionC_msg = Float64()
 
-        thrustA_msg.data = thrust_to_rotations(msgs[0])
-        directionA_msg.data = np.radians(msgs[1])
-        thrustB_msg.data = thrust_to_rotations(msgs[2])
-        directionB_msg.data = np.radians(msgs[3])
-        thrustC_msg.data = thrust_to_rotations(msgs[4])
-        directionC_msg.data = np.radians(msgs[5])
+
+        thrustA, thetaA = cartesian_to_polar(msgs[0], msgs[1])
+        thrustB, thetaB = cartesian_to_polar(msgs[2], msgs[3])
+        thrustC, thetaC = cartesian_to_polar(msgs[4], msgs[5])
+
+        thrustA_msg.data = thrust_to_rotations(thrustA)
+        directionA_msg.data = thetaA
+        thrustB_msg.data = thrust_to_rotations(thrustB)
+        directionB_msg.data = thetaB
+        thrustC_msg.data = thrust_to_rotations(thrustC)
+        directionC_msg.data = thetaC
 
         self.thrustA_publisher.publish(thrustA_msg)
         self.thrustB_publisher.publish(thrustB_msg)
@@ -81,14 +108,15 @@ class VerenikiControllerNode(Node):
         self.steerB_publisher.publish(directionB_msg)
         self.steerC_publisher.publish(directionC_msg)
 
-        self.get_logger().info("Thrust info (thrust, deg): \n"
-                               f"A: {msgs[0]} {msgs[1]}\n"
-                               f"B: {msgs[2]} {msgs[3]}\n"
-                               f"C: {msgs[4]} {msgs[5]}")
+        # self.get_logger().info(str(msgs))
+        # self.get_logger().info("Thrust info (thrust, deg): \n"
+        #                        f"A: {msgs[0]} {msgs[1]}\n"
+        #                        f"B: {msgs[2]} {msgs[3]}\n"
+        #                        f"C: {msgs[4]} {msgs[5]}")
         self.get_logger().info("Thrust info (rad/s, radians): \n"
-                               f"{thrust_to_rotations(msgs[0])} {np.radians(msgs[1])}\n"
-                               f"{thrust_to_rotations(msgs[2])} {np.radians(msgs[3])}\n"
-                               f"{thrust_to_rotations(msgs[4])} {np.radians(msgs[5])}")
+                               f"{thrustA} {thetaA}\n"
+                               f"{thrustB} {thetaB}\n"
+                               f"{thrustC} {thetaC}")
 
 
 def main():
