832f554195f158f1fcccf9f6fea4420942fee4b5
g-poulos <cs04480@uoi.gr>
2024-01-15 14:02:23 +0200

    Fixed yaw

--- sim_scripts/compare_results.py
+++ sim_scripts/compare_results.py
@@ -12,19 +12,20 @@ def get_mcap_messages(file):
     for msg in read_ros2_messages(file):
         if msg.channel.topic == "/model/vereniki/odometry":
             # print(f"{msg.ros_msg.pose.pose.position.x}: \n")
+            yaw = quaternion_to_yaw([msg.ros_msg.pose.pose.orientation.w,
+                                     msg.ros_msg.pose.pose.orientation.x,
+                                     msg.ros_msg.pose.pose.orientation.y,
+                                     msg.ros_msg.pose.pose.orientation.z])
+
             pos = np.column_stack((pos, np.array([msg.ros_msg.pose.pose.position.x,
                                                   msg.ros_msg.pose.pose.position.y,
-                                                  msg.ros_msg.pose.pose.orientation.z])))
+                                                  yaw])))
             vel = np.column_stack((vel, np.array([msg.ros_msg.twist.twist.linear.x,
                                                   msg.ros_msg.twist.twist.linear.y,
                                                   msg.ros_msg.twist.twist.angular.z])))
 
         temp_acc = np.zeros(3)
         if msg.channel.topic == "/model/vereniki/acceleration/linear":
-            # print(f"{msg.ros_msg.linear_acceleration.x}")
-            # acc = np.column_stack((acc, np.array([msg.ros_msg.x,
-            #                                       msg.ros_msg.y,
-            #                                       0])))
             temp_acc[0] = msg.ros_msg.x
             temp_acc[1] = msg.ros_msg.y
 
@@ -32,7 +33,6 @@ def get_mcap_messages(file):
             temp_acc[2] = msg.ros_msg.z
 
         acc = np.column_stack((acc, temp_acc))
-
 
     odom_data = {'Position-x': pos[0, :],
                  'Position-y': pos[1, :],
@@ -52,10 +52,15 @@ def align_values(data, size):
     return data.iloc[selected_indices].reset_index(drop=True)
 
 
-def radians_to_degrees(radians):
-    degrees = np.degrees(radians)
-    degrees = (degrees + 180) % 360 - 180
-    return degrees
+def quaternion_to_yaw(quaternion):
+    # Extract the relevant quaternion components
+    q0, q1, q2, q3 = quaternion
+
+    # Calculate yaw (rotation around the vertical axis)
+    yaw = np.arctan2(2 * (q0 * q3 + q1 * q2), 1 - 2 * (q2**2 + q3**2))
+
+    # Convert yaw from radians to degrees
+    return np.degrees(yaw)
 
 
 def plot3_1(diff_values, sim_values, title):
@@ -75,19 +80,19 @@ def plot3_1(diff_values, sim_values, title):
         ax[i].grid(True)
         ax[i].legend(loc="best")
 
-    return ax, fig
+    return fig, ax
 
 
 if __name__ == '__main__':
     # Read simulation data
-    odom_data, imu_data = get_mcap_messages("../bagfiles/record7/record7_0.mcap")
+    odom_data, imu_data = get_mcap_messages("../bagfiles/record9/record9_0.mcap")
     odom_data_size = odom_data.shape[0]
     imu_data_size = imu_data.shape[0]
 
     # Read dynamic model data
     diff_data = pd.read_csv('output.csv')
 
-    # Align value to
+    # Align Gazebo simulation and Dynamic Model values
     x_pos_diff = align_values(diff_data['Position-x'], odom_data_size)
     y_pos_diff = align_values(diff_data['Position-y'], odom_data_size)
     z_orient_diff = align_values(diff_data['Orientation-z'], odom_data_size)
@@ -100,6 +105,7 @@ if __name__ == '__main__':
     y_acc_diff = align_values(diff_data['Acceleration-y'], imu_data_size)
     z_acc_diff = align_values(diff_data['Acceleration-z'], imu_data_size)
 
+    # Create separate dataframes
     pose_diff = pd.DataFrame({'Position-x': x_pos_diff,
                               'Position-y': y_pos_diff,
                               'Orientation-z': z_orient_diff})
@@ -113,11 +119,28 @@ if __name__ == '__main__':
                              'Acceleration-z': z_acc_diff})
 
     odom_data = pd.DataFrame(odom_data)
-    odom_data['Orientation-z'] = odom_data['Orientation-z'].apply(radians_to_degrees)
+    # odom_data['Orientation-z'] = odom_data['Orientation-z'].apply(radians_to_degrees)
     imu_data = pd.DataFrame(imu_data)
 
-    plot3_1(pose_diff, odom_data, title="Position and Orientation")
-    plot3_1(twist_diff, odom_data.iloc[:, 3:], title="Linear and Angular Velocity")
-    plot3_1(acc_diff, imu_data, title="Linear and Angular Acceleration")
+    # Plot results
+    plt.rcParams["font.family"] = "Times New Roman"
+    plt.rcParams.update({'font.size': 15})
+    pose_fig, pose_ax = plot3_1(pose_diff, odom_data, title="Position and Orientation")
+    twist_fig, twist_ax = plot3_1(twist_diff, odom_data.iloc[:, 3:], title="Linear and Angular Velocity")
+    acc_fig, acc_ax = plot3_1(acc_diff, imu_data, title="Linear and Angular Acceleration")
 
+    pose_ax[0].set_ylabel('X-Axis Position [m]')
+    pose_ax[1].set_ylabel('Y-Axis Position [m]')
+    pose_ax[2].set_ylabel('Z-Axis Orientation [deg]')
+    pose_ax[2].set_xlabel('Time [s]')
+
+    twist_ax[0].set_ylabel('X-Axis \nLinear Velocity [m/s]')
+    twist_ax[1].set_ylabel('Y-Axis \nLinear Velocity [m/s]')
+    twist_ax[2].set_ylabel('Z-Axis \nAngular Velocity [rad/s]')
+    twist_ax[2].set_xlabel('Time [s]')
+
+    acc_ax[0].set_ylabel('X-Axis Linear\nAcceleration [m/s^2]')
+    acc_ax[1].set_ylabel('Y-Axis Linear\nAcceleration [m/s^2]')
+    acc_ax[2].set_ylabel('Z-Axis Angular\nAcceleration [rad/s^2]')
+    acc_ax[2].set_xlabel('Time [s]')
     plt.show()
